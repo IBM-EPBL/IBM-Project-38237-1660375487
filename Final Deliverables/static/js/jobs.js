55df8203461f38a7ba1feb40499ceb603c74ed0b
AGNES LILY <97900099+AgnesLily2772@users.noreply.github.com>
2022-11-19 23:26:43 +0530

    Add files via upload

---
+++ Final Deliverables/static/js/jobs.js
@@ -0,0 +1,22 @@
+import data from '../index.json' assert { type: "json" };
+
+let html=""
+data.map((item,index)=>{
+   let itemDate = new Date(item.postedAt)
+   let jobrole = item.jobRole
+    html+=`
+    <div class="jobCard" accesskey=${index}>
+        <h6><i class="bi bi-person-badge"></i>       ${item.jobRole}</h6>
+        <p><i class="bi bi-building"></i>       ${item.companyName}</p>
+        <p><i class="bi bi-calendar-check"></i>       ${itemDate.toLocaleString().slice(0,9)}</p>
+        <a type="button" class="btn btn-success"  href="/viewDetail/${item.id}">
+        View JD
+      </a>
+      <a type="button" class="btn btn-danger"  href="/applyJob/${item.id}">
+      Apply Job
+    </a>                    
+    </div>
+    
+    `
+})
+document.getElementById("main").innerHTML = html
