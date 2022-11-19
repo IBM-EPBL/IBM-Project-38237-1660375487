a441e275c2193afa4bd0da0b5f3c8c73fcf2d242
AGNES LILY <97900099+AgnesLily2772@users.noreply.github.com>
2022-11-19 18:17:10 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 2/static/js/JobDetail.js
@@ -0,0 +1,49 @@
+import data from '../index.json' assert { type: "json" };
+
+let index = document.getElementById("mainthing").value
+let jobdesc = data[index-1]
+
+let pair = {
+    0:"HTML,CSS,JS",
+    1:"Communication",
+    2:"Python",
+    3:"C,C++",
+    4:"Java",
+    5:"UI/UX",
+    6:"Node JS",
+    7:"React JS",
+    8:"MySQL",
+    9:"AWS"
+}
+let skillsetString = jobdesc.skill.toString()
+console.log(typeof(skillsetString))
+let  skillString = skillsetString.split('')
+console.log(skillString)
+let detail  = ""
+detail+=`
+<div>
+    <h2>Job Role : ${jobdesc.jobRole}</h2>
+    <p>Company Name : ${jobdesc.companyName}</p>
+    <p>Job Location : ${jobdesc.jobLocation}</p>
+    <p>Job Description : ${jobdesc.jobDescription}</p>
+    <p>Posted At: ${jobdesc.postedAt.toLocaleString().slice(0,10)}</p>
+
+
+</div>
+`
+
+let str = ""
+skillString.map((item)=>{
+    console.log(item)
+    str+=`
+    <li>${pair[item]}</li>
+`
+})
+
+let links = ""
+links+=`
+    <a href="/viewJobs" class="btn btn-success" >Apply Here</a>
+`
+document.getElementById("applyLink").innerHTML = links
+document.getElementById("skillss").innerHTML = str
+document.getElementById("jobDetailBox").innerHTML =detail
---
+++ Project Development Phase/Sprint 2/static/js/navbar.js
@@ -0,0 +1,30 @@
+let flag = false
+let valuee = sessionStorage.getItem("IBM_WAC_DEVICE_ID")
+
+if(valuee == null)
+{
+    flag = false
+}
+else {
+    flag = true
+}
+
+let listitems = ""
+if(flag === true)
+{
+    listitems+=`
+        <a href="/profile" class="btn btn-info me-3">Profile</a>
+        <a href="/viewJobs" class="btn btn-info me-3">View Jobs</a>
+        <a href="/logout" class="btn btn-info me-3">Logout</a>      
+    `
+}
+else if(flag===false){
+    listitems+=`
+        <a href="/register" class="btn btn-info me-3">Register</a>
+        <a href="/login" class="btn btn-info me-3">Login</a>
+    `
+}
+
+document.getElementById("navlist").innerHTML = listitems
+
+
